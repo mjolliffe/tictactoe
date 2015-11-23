8f12e537ee9656cdcdfbd836060540c32effbd02
Michelle Jolliffe <michelle_jolliffe@alumni.brown.edu>
2015-11-22 17:47:56 -0800

    Finished basic game functionality, but need to add listeners to change color of tiles. Need to clean up CSS.

--- script.js
+++ script.js
@@ -1,8 +1,98 @@
-function move(){
-  document.getElementByClass("square").innerHTML = "X";
+var player = 1;
+
+function clickBtn(btn) {
+  if (player===1){
+  document.getElementById(btn).value = "X";
+  document.getElementById(btn).disabled = "disabled";
+  player -= 1;
+  winner();
+  } else {
+  document.getElementById(btn).value = "O";
+  document.getElementById(btn).disabled = "disabled";
+  player += 1;
+  winner();
+  }
 }
 
+function winner(){
+  if (
+  document.getElementById("s1").value == "X" &&
+  document.getElementById("s2").value == "X" &&
+  document.getElementById("s3").value == "X" ||
+  document.getElementById("s4").value == "X" &&
+  document.getElementById("s5").value == "X" &&
+  document.getElementById("s6").value == "X" ||
+  document.getElementById("s7").value == "X" &&
+  document.getElementById("s8").value == "X" &&
+  document.getElementById("s9").value == "X" ||
+  document.getElementById("s1").value == "X" &&
+  document.getElementById("s4").value == "X" &&
+  document.getElementById("s7").value == "X" ||
+  document.getElementById("s2").value == "X" &&
+  document.getElementById("s5").value == "X" &&
+  document.getElementById("s8").value == "X" ||
+  document.getElementById("s3").value == "X" &&
+  document.getElementById("s6").value == "X" &&
+  document.getElementById("s9").value == "X" ||
+  document.getElementById("s1").value == "X" &&
+  document.getElementById("s5").value == "X" &&
+  document.getElementById("s9").value == "X" ||
+  document.getElementById("s3").value == "X" &&
+  document.getElementById("s5").value == "X" &&
+  document.getElementById("s7").value == "X"
+    ){
+  alert("The Winner is X!");
+  resetTable();
+  } else if (
+  document.getElementById("s1").value == "O" &&
+  document.getElementById("s2").value == "O" &&
+  document.getElementById("s3").value == "O" ||
+  document.getElementById("s4").value == "O" &&
+  document.getElementById("s5").value == "O" &&
+  document.getElementById("s6").value == "O" ||
+  document.getElementById("s7").value == "O" &&
+  document.getElementById("s8").value == "O" &&
+  document.getElementById("s9").value == "O" ||
+  document.getElementById("s1").value == "O" &&
+  document.getElementById("s4").value == "O" &&
+  document.getElementById("s7").value == "O" ||
+  document.getElementById("s2").value == "O" &&
+  document.getElementById("s5").value == "O" &&
+  document.getElementById("s8").value == "O" ||
+  document.getElementById("s3").value == "O" &&
+  document.getElementById("s6").value == "O" &&
+  document.getElementById("s9").value == "O" ||
+  document.getElementById("s1").value == "O" &&
+  document.getElementById("s5").value == "O" &&
+  document.getElementById("s9").value == "O" ||
+  document.getElementById("s3").value == "O" &&
+  document.getElementById("s5").value == "O" &&
+  document.getElementById("s7").value == "O" )
+  {
+  alert("The Winner is O!");
+  resetTable();
+  }
+}
 
-function resetTable() {
-    document.getElementById("reset").innerHTML = "";
+element.addEventListener(event, function, useCapture)
+
+function resetTable (){
+  document.getElementById("s1").value = "";
+  document.getElementById("s2").value = "";
+  document.getElementById("s3").value = "";
+  document.getElementById("s4").value = "";
+  document.getElementById("s5").value = "";
+  document.getElementById("s6").value = "";
+  document.getElementById("s7").value = "";
+  document.getElementById("s8").value = "";
+  document.getElementById("s9").value = "";
+  document.getElementById("s1").disabled = "";
+  document.getElementById("s2").disabled = "";
+  document.getElementById("s3").disabled = "";
+  document.getElementById("s4").disabled = "";
+  document.getElementById("s5").disabled = "";
+  document.getElementById("s6").disabled = "";
+  document.getElementById("s7").disabled = "";
+  document.getElementById("s8").disabled = "";
+  document.getElementById("s9").disabled = "";
 }
